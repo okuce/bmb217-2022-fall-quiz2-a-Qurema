cc70b2cb496c95e4aa3518bb5923140652206017
Qurema <113541513+Qurema@users.noreply.github.com>
2022-10-06 14:08:33 +0300

    Update Otobus.py

--- Otobus.py
+++ Otobus.py
@@ -1,18 +1,34 @@
 class Otobus:
-    """Otobus bilet satis takip sinifi"""
-    
-
-    def bilet_sat(self):
-        """Otobusteki dolu koltuk sayisini 1 artirir"""
+    plaka=""
+    Nereden=""
+    Nereye=""
+    Boskoltuk=0
+    Dolukoltuk=0
+    def __init__(self,plaka,Nereden,Nereye,Boskoltuk,Dolukoltuk):
+        self.plaka= plaka
+        self.Nereden= Nereden
+        self.Nereye= Nereye 
+        self.Bsokoltuk=Boskoltuk
+        self.Dolukoltuk= Dolukoltuk
+        
+        
+    def bilet_sat(self,Dolukoltuk):
+        koltuk=0
+        koltuk= (self.Dolukoltuk + 1)
+        return koltuk
         
     
-    def bilet_iade(self):
-        """Otobusteki dolu koltuk sayisini 1 azaltir"""
+    def bilet_iade(self,Boskoltuk):
+        boskoltuk=0
+        boskoltuk= (koltuk-1)
+        return boskoltuk
         
+        
+        
+        """Otobusun guzergahini, plakasini,bos ve dolu koltuk sayisini yazdirir"""
 
     
     def durum_yaz(self):
-        """Otobusun guzergahini, plakasini,bos ve dolu koltuk sayisini yazdirir"""
-        
+        print("Nereden:{}, Nereye:{}, Plaka:{}, Dolukoltuk:{}, Boskoltuk:{}".format(self.Nereden, self.Nereye, self.Plaka, koltuk, boskoltuk) )
 
 
